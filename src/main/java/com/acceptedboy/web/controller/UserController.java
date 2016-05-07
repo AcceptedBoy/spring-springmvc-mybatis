44d9ecb5995d2af82150f6b29f49d6a499be24ee
AcceptedBoy <972192420@qq.com>
2016-05-07 14:58:54 +0800

    jackson返回修正

--- src/main/java/com/acceptedboy/web/controller/UserController.java
+++ src/main/java/com/acceptedboy/web/controller/UserController.java
@@ -1,13 +1,14 @@
 package com.acceptedboy.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.acceptedboy.po.User;
 import com.acceptedboy.service.UserService;
 
-@Controller
+@RestController
 public class UserController {
 	
 	@Autowired
@@ -16,7 +17,10 @@ public class UserController {
 	@RequestMapping("/getUser")
 	public Object getUser(@RequestParam(required=false)String id) {
 		
-		System.out.println(userService.selectByPrimaryKey(id));
-		return "index";
+		User user = new User();
+		user.setId("12");
+		user.setUsername("32");
+		user.setPassword("111");
+		return user;
 	}
 }
